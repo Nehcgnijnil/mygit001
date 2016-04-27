505005e6f29e3fac612897a8bae0fcd79d58540b
qianfeng <qianfeng@shishishangshishishang.local>
2016-04-27 11:41:18 +0800

    commit rongqi

---
+++ rongqi/01.容器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  01.容器
+//
+//  Created by qianfeng on 16/4/12.
+//  Copyright (c) 2016年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
